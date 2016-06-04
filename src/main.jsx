077395cd669f44bbcbba423af08ad296b0e6f074
Michael-Bryant Choa <mbchoa@gmail.com>
2016-06-04 14:33:51 -0700

    add socket event handling for play, add cors

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import SongPlayer from './components/SongPlayer.jsx'
 import Songs from './components/Songs.jsx'
 import ReactAudioPlayer from 'react-audio-player';
 
-const socket = io();
-
 class Slick extends React.Component {
   constructor() {
     //initial state is an empty array
@@ -18,28 +16,52 @@ class Slick extends React.Component {
     };
     this.newSongClick = this.newSongClick.bind(this);
     this.onPlay = this.onPlay.bind(this);
+    this.updateSong = this.updateSong.bind(this);
   }
-  
+
+
   newSongClick(i) {
+    // get song object from index
+    const songObj = this.state.songInfo[i];
+
+    // get song url from object
+    const songUrl = songObj.trackUrl;
+
+    // emit playSong event
+    this.socket.emit('playSong', songUrl);
+  }
+
+  handleServerPlayEvent(songUrl) {
+    this.updateSong(songUrl);
+  }
+
+  updateSong(url) {
+    const index = this.state.songInfo.indexOf()
+    for (var i = 0; i < this.state.songInfo.length; i++) {
+      if (this.state.songInfo[i].trackUrl === url)
+        break;
+    }
+
     let nextSong = this.state.songInfo.splice(i, 1);
     this.setState({
       firstSong: nextSong[0],
       songInfo: this.state.songInfo
     });
-
-    // emit playSong event
   }
 
   onPlay() {
 
   }
 
+
+
   //doing async request in cdm
   componentDidMount() {
+    this.socket = io();
     let that = this;
     $.ajax({
       method: 'GET',
-      url: "http://localhost:3000/songQueue",
+      url: "http://192.168.1.19:3000/songQueue",
       contentType: 'application/json',
       dataTyle: 'json',
       success: data => {
@@ -48,6 +70,7 @@ class Slick extends React.Component {
           songInfo: data
         });
         // listen for playSong emit events
+        this.socket.on('playSong', handleServerPlayEvent);
       }
     });
   }
