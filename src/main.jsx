3179676caafb2cab6e60792120471200ed63beb6
Jerry Mao <jerrymao@Jerrys-MacBook-Air.local>
2016-06-04 00:58:16 -0700

    ditched angular. set up boilerplate for react. we're stoked we don't have to mess with angular anymore :)

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class Slick extends React.Component {
+  constructor() {
+    this.state = {};
+  }
+
+
+  render() {
+    return (
+      <div>
+        <p>testing</p>
+      </div>
+    )
+  }
+
+}
+
+
+ReactDOM.render(
+  <Slick />,
+  document.getElementById('content')
+)
