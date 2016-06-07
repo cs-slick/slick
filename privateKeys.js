cb03aa385230bae539f8cb4853c49e932def3136
anonymous <anonymous@email.com>
2016-06-07 15:53:24 -0700

    private keys for spotify

--- privateKeys.js
+++ privateKeys.js
@@ -4,6 +4,9 @@ const privateKeys = {
   clientId: '1064631179',
   clientTag: 'D53C77DB2157936F00C4BE8D2AADAF12',
   userId: '87071795693237026-C14D11FA6E840FCCE3D1129E34AC8208',
+
+  ClientIdSpotify: "a22cb34e6ab84becac3a72b819ec4f67",
+  ClientSecretSpotify: "b2644472d210462c816ea397060c99c6",
   // Database
   postgresURI: 'postgres://usewxxncwyaejx:tZJCuwYOCU0QArl3JUv9_gyE-n@ec2-23-21-164-237.compute-1.amazonaws.com:5432/d4imhd4libvku4'
 };
